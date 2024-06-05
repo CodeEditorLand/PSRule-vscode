6a8e4f6b438e4cef3707da509d6c11975dfccdb8
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-06-06 01:33:16 +0300

    squash!

--- src/consts.ts
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-'use strict';
-
-export const defaultOptionsFile: string = 'ps-rule.yaml';
-export const configurationItemPrefix: string = 'PSRule';
